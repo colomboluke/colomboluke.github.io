a9056cd8aa36c41767a5c27e75c5643d69230cf9
Luke Colombo <123432468+lukecolombo03@users.noreply.github.com>
2024-02-01 21:03:03 -0500

    Add files via upload

---
+++ website/functions.js
@@ -0,0 +1,21 @@
+let toTopButton = document.getElementById("to-top-button");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    toTopButton.style.display = "block";
+  } else {
+    toTopButton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
